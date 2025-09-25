3ceecbba6ac168281072058601968ec6290844dd
Mapute_N/github_username <novymapute@gmail.com>
2025-09-25 16:32:12 +0800

    Update files

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
-    import ColorChangerApp from './ColorChangerApp';
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import CounterApp from './CounterApp';  
-
+import CommentApp from './CommentApp';
+import MessengerApp from './MessengerApp';
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-     <CounterApp />
-      <ColorChangerApp />
- 
+     <MessengerApp/>
+    <CommentApp/>
     </SafeAreaView>
   );
 }
